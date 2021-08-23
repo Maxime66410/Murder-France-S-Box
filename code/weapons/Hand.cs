3ec9bc576baa6bf39d5c29df543efa3b69decf7e
Maxime66410 <max66410@hotmail.fr>
2021-08-23 23:45:52 +0200

    ...

--- code/weapons/Hand.cs
+++ code/weapons/Hand.cs
@@ -1,7 +1,7 @@
 ﻿using Sandbox;
 using System;
 
-[Library("weapon_zombiehand", Title = "HandZombie", Spawnable = false)]
+[Library("weapon_hand", Title = "Hand", Spawnable = false)]
 [Hammer.EditorModel("weapons/rust_boneknife/rust_boneknife.vmdl")]
 partial class ZombieHand : BaseDmWeapon
 {
@@ -50,7 +50,6 @@ partial class ZombieHand : BaseDmWeapon
 	{
 		//if (!CanPrimaryAttack()) return;
 
-		RandomSoundOnFire();
 		
 		MeleeStrike(BaseDamage, 1.5f);
 		
@@ -66,7 +65,6 @@ partial class ZombieHand : BaseDmWeapon
 		(Owner as AnimEntity).SetAnimBool("b_attack", true);
 
 		ShootEffects();
-		RandomSoundOnFire();
 		MeleeStrike(BaseDamage * 1.5f, 1.5f);
 	}
 
@@ -83,67 +81,5 @@ partial class ZombieHand : BaseDmWeapon
 	{
 		anim.SetParam("holdtype", 4); // TODO this is shit
 		anim.SetParam("aimat_weight", 1.0f);
-	}
-
-	public void RandomSoundOnFire()
-	{
-		Random rnd = new Random();
-		
-		var RandomSound = rnd.Next( 0, 10 );
-
-		if (RandomSound == 0)
-		{
-			PlaySound("rust_boneknife_zombie_1.attack");
-		}
-
-		if ( RandomSound == 1 )
-		{
-			PlaySound("rust_boneknife_zombie_2.attack");
-		}
-		
-		if ( RandomSound == 2 )
-		{
-			PlaySound("rust_boneknife_zombie_3.attack");
-		}
-		
-		if ( RandomSound == 3 )
-		{
-			PlaySound("rust_boneknife_zombie_4.attack");
-		}
-		
-		if ( RandomSound == 4 )
-		{
-			PlaySound("rust_boneknife_zombie_5.attack");
-		}
-		
-		if ( RandomSound == 5 )
-		{
-			PlaySound("rust_boneknife_zombie_6.attack");
-		}
-		
-		if ( RandomSound == 6 )
-		{
-			PlaySound("rust_boneknife_zombie_7.attack");
-		}
-		
-		if ( RandomSound == 7 )
-		{
-			PlaySound("rust_boneknife_zombie_8.attack");
-		}
-		
-		if ( RandomSound == 8 )
-		{
-			PlaySound("rust_boneknife_zombie_9.attack");
-		}
-		
-		if ( RandomSound == 9 )
-		{
-			PlaySound("rust_boneknife_zombie_10.attack");
-		}
-		
-		if ( RandomSound == 10 )
-		{
-			PlaySound("rust_boneknife_zombie_11.attack");
-		}
 	}
 }
